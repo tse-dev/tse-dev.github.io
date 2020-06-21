c1cc3cbb57715d31c36f3ac148d4433f50997c10
Daniel Loth <lothyis@gmail.com>
2020-06-21 14:20:41 +1000

    Remove public folder files from version control

--- public/component---src-pages-404-js-fc8718ffb723b3b8af10.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),u=e.n(o);t.default=function(){return u.a.createElement("h1",null,"404 - Not found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-fc8718ffb723b3b8af10.js.map
